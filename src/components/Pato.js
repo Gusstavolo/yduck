134ef1a91c7633d65da04ce771dbc12f9a38db57
Gustavo Oli <69ggustavoo69@gmail.com>
2024-04-23 11:25:43 -0300

    ad pato

---
+++ src/components/Pato.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+
+export function Pato(){
+    
+
+
+    return(
+        <div className='pato'>
+            
+        </div>
+    );
+}
--- src/components/world.js
+++ src/components/world.js
@@ -1,5 +1,5 @@
 import '../App.css';
-
+import { Pato } from './Pato';
 export function World() {
 
 
@@ -7,6 +7,12 @@ export function World() {
     return (
 
         <div className='world'>
+
+
+        <Pato></Pato>
+
+
+
 
         </div>
 
