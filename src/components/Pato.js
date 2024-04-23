f3ff87a83c7be9d360e78f8fd3c09c40357c23b7
Gustavo Oli <69ggustavoo69@gmail.com>
2024-04-23 11:26:06 -0300

    start script move

--- src/components/Pato.js
+++ src/components/Pato.js
@@ -1,13 +1,32 @@
-import '../App.css';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 
+export function Pato() {
+  const [position, setPosition] = useState({ top: 0, left: 0, rotate: 0 });
 
-export function Pato(){
+  useEffect(() => {
     
+  });
 
+  const handleClick = () => {
+    // Atualiza a posição
+    setPosition({
+        top: position.top + 10,
+        left: position.left + 10,
+        rotate: position.rotate + "330deg",
+      });
+  };
 
-    return(
-        <div className='pato'>
-            
-        </div>
-    );
+  return (
+    <div 
+      onClick={handleClick} 
+      className='pato'
+      style={{
+        position: 'absolute',
+        top: position.top,
+        left: position.left,
+        rotate: position.rotate,
+      }}
+    />
+  );
 }
--- src/components/world.js
+++ src/components/world.js
@@ -1,18 +1,30 @@
 import '../App.css';
 import { Pato } from './Pato';
+import React from 'react';
+import { useId, useState } from 'react';
+
+
+
+
 export function World() {
 
+    const [position, setPosition] = useState({ top: 0, left: 0 });
 
+    const handleClick = () => {
+      // Atualiza a posição
+      setPosition({
+        top: position.top + 10+ "px",
+        left: position.left + 10 + "px",
+      });
+    };
 
     return (
 
         <div className='world'>
 
-
-        <Pato></Pato>
-
-
-
+            <div className='worldSpace'>
+                  <Pato onClick={handleClick}  ></Pato>
+            </div>
 
         </div>
 
