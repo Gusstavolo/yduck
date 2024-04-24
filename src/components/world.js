8a1ac60a6d996e97a3ed8329d0d165fc2135dd71
Gustavo Oli <69ggustavoo69@gmail.com>
2024-04-24 10:56:10 -0300

    add box generate

--- src/components/world.js
+++ src/components/world.js
@@ -17,8 +17,18 @@ export function World() {
                   
                   <Pato ></Pato>
                   <Pato ></Pato>
-            </div>
 
+            </div>
+            <div className='config_generate_box'>
+                <div className='generate_box'>
+                    <div >NOME</div>
+                    <input className='generate_box_name' type="text" name="name"></input>
+                    <div>MENSAGEM </div>
+                    <input className='generate_box_msm' type="text" name="name"></input>
+                    
+                    <input className='generate_box_button' type="submit" value="Submit" />
+                </div>
+            </div>
         </div>
 
     );
